c83e56877324aa89475f69b011d18cfdb18d0e37
Maxim Cherepanov <m.cherepanov@dokltd.ru>
2020-11-11 11:17:22 +0300

    Open function added

--- notepad.py
+++ notepad.py
@@ -1,37 +1,68 @@
 import wx
 import os
- 
+
+WIDTH = 500
+HEIGHT = 400
+
 class Window(wx.Frame):
  
     def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(300,250))
+        wx.Frame.__init__(self, parent, title=title, size=(WIDTH, HEIGHT))
         self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
         self.Show(True)
- 
-        menu = wx.Menu()
-        aboutItem = menu.Append(wx.ID_ABOUT,"About","Push the button to get an information about this application")
-        openItem = menu.Append(wx.ID_OPEN,"Open","Push the button to Open file")
-        exitItem = menu.Append(wx.ID_EXIT,"Exit","Push the button to leave this application")
-        bar = wx.MenuBar()
         
-        bar.Append(menu,"File")
-        self.SetMenuBar(bar)
+        
+        menubar = wx.MenuBar()
+ 
+        file_menu = wx.Menu()
+        openItem = file_menu.Append(wx.ID_OPEN,"Open","Push the button to Open file")
+        saveItem = file_menu.Append(wx.ID_SAVE,"Save","Push the button to Save file")
+        exitItem = file_menu.Append(wx.ID_EXIT,"Exit","Push the button to leave this application")
+        #bar = wx.MenuBar()
+        menubar.Append(file_menu,"File")
+        self.SetMenuBar(menubar)
+        self.Bind(wx.EVT_MENU, self.OnOpen, openItem)
+        self.Bind(wx.EVT_MENU, self.OnSave, saveItem)
+        self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
+        
+        
+        help_menu = wx.Menu()
+        aboutItem = help_menu.Append(wx.ID_ABOUT,"About","Push the button to get an information about this application")
+        menubar.Append(help_menu,"Help")
         self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
         
-        self.Bind(wx.EVT_MENU, self.OnExit, openItem)
-        #bar.Append(menu,"Open")
-        #self.SetMenuBar(bar)
-        self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
+        
+        
  
     def OnAbout(self, e):
         dlg = wx.MessageDialog(self, "This is a mini editor keeping your text","About pyNote", wx.OK)
         dlg.ShowModal()
     
     def OnOpen(self, e):
-        exit()
+        with wx.FileDialog(self, "Open TXT file", wildcard="TXT files (*.txt)|*.txt",
+                       style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+            
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return     # the user changed their mind
+            
+            # Proceed loading the file chosen by the user
+            pathname = fileDialog.GetPath()
+            try:
+                with open(pathname, 'r') as file:
+                    self.LoadFile(file)
+            except IOError:
+                wx.LogError("Cannot open file '%s'." % newfile)
+    
+    def OnSave(self, e):
+        pass
     
     def OnExit(self, e):
         exit()
+    
+    def LoadFile(self, file):
+        
+        self.control.AppendText(file.read())
+        #self.control.AppendText("Some another Text\n")
 
 app = wx.App()
 wnd = Window(None, "pyNote")
